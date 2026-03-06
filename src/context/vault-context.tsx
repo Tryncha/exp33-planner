e7b3a07f17151a8a86f29a4701cae26e01083976
Tryncha <juanes.aguirre26@gmail.com>
2026-03-06 14:27:02 -0500

    teams feature initialized, need improves

--- src/context/vault-context.tsx
+++ src/context/vault-context.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { Build } from '../types';
+import { Build, Team } from '../types';
 
 interface VaultContextType {
   vault: Build[];
@@ -21,41 +21,65 @@ export function useVault() {
 }
 
 export const VaultProvider = ({ children }: { children: React.ReactNode }) => {
-  const [vault, setVault] = useState<Build[]>([]);
+  const [builds, setBuilds] = useState<Build[]>([]);
+  const [teams, setTeams] = useState<Team[]>([]);
 
   useEffect(() => {
     try {
       const savedVault = localStorage.getItem('vault');
-      if (savedVault) setVault(JSON.parse(savedVault));
+      if (savedVault) setBuilds(JSON.parse(savedVault));
     } catch {}
   }, []);
 
   useEffect(() => {
     try {
-      localStorage.setItem('vault', JSON.stringify(vault));
+      localStorage.setItem('vault', JSON.stringify(builds));
     } catch {}
-  }, [vault]);
+  }, [builds]);
 
   function addBuild(newBuild: Build) {
-    setVault(vault.concat(newBuild));
+    setBuilds(builds.concat(newBuild));
   }
 
   function updateBuild(buildId: Build['id'], newBuild: Build) {
-    const buildToUpdate = vault.find((bld) => bld.id === buildId);
+    const buildToUpdate = builds.find((bld) => bld.id === buildId);
 
     if (!buildToUpdate) {
       throw new Error(`Build with id: ${buildId} not found...`);
     }
 
-    setVault(vault.with(vault.indexOf(buildToUpdate), newBuild));
+    setBuilds(builds.with(builds.indexOf(buildToUpdate), newBuild));
   }
 
   function removeBuild(buildId: Build['id']) {
-    setVault(vault.filter((bld) => bld.id !== buildId));
+    setBuilds(builds.filter((bld) => bld.id !== buildId));
+  }
+
+  function addBuildToTeam(buildId: Build['id'], teamId: Team['id']) {
+    const buildToAdd = builds.find((bld) => bld.id === buildId);
+
+    if (!buildToAdd) {
+      throw new Error(`Build with id: ${buildId} not found...`);
+    }
+
+    const teamToUpdate = teams.find((team) => team.id === teamId);
+
+    if (!teamToUpdate) {
+      throw new Error(`Team with id: ${teamId} not found...`);
+    }
+
+    setTeams(
+      teams.with(teams.indexOf(teamToUpdate), {
+        ...teamToUpdate,
+        builds: teamToUpdate.builds.concat(buildToAdd) as [Build, Build, Build]
+      })
+    );
   }
 
   return (
-    <VaultContext.Provider value={{ vault, addBuild, updateBuild, removeBuild }}>{children}</VaultContext.Provider>
+    <VaultContext.Provider value={{ vault: builds, addBuild, updateBuild, removeBuild }}>
+      {children}
+    </VaultContext.Provider>
   );
 };
 
